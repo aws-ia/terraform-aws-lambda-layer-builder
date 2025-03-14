2fe3aee1e47a3aa0c85d3da93d12c722cb3f1941
Troy Ameigh <55170274+troy-ameigh@users.noreply.github.com>
2025-03-14 13:25:03 -0500

    added tests to repo

--- project/test/examples_basic_test.go
+++ project/test/examples_basic_test.go
@@ -1,21 +1,96 @@
 package test
 
 import (
-	"testing"
-
-	"github.com/gruntwork-io/terratest/modules/terraform"
+    "testing"
+    "github.com/gruntwork-io/terratest/modules/terraform"
+    "github.com/stretchr/testify/assert"
 )
 
-func TestExamplesBasic(t *testing.T) {
+func TestVariablesValidation(t *testing.T) {
+    t.Run("valid_lambda_architecture", func(t *testing.T) {
+        terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+            TerraformDir: "../",
+            Vars: map[string]interface{}{
+                "lambda_function_architecture": "x86_64",
+            },
+        })
+        
+        terraform.Init(t, terraformOptions)
+        terraform.Plan(t, terraformOptions)
+    })
 
-	terraformOptions := &terraform.Options{
-		TerraformDir: "../examples/basic",
-		// Vars: map[string]interface{}{
-		// 	"myvar":     "test",
-		// 	"mylistvar": []string{"list_item_1"},
-		// },
-	}
+    t.Run("invalid_lambda_architecture", func(t *testing.T) {
+        terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+            TerraformDir: "../",
+            Vars: map[string]interface{}{
+                "lambda_function_architecture": "invalid_arch",
+            },
+        })
+        
+        _, err := terraform.InitAndPlanE(t, terraformOptions)
+        assert.Error(t, err)
+    })
 
-	defer terraform.Destroy(t, terraformOptions)
-	terraform.InitAndApply(t, terraformOptions)
+    t.Run("valid_lambda_runtime", func(t *testing.T) {
+        terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+            TerraformDir: "../",
+            Vars: map[string]interface{}{
+                "lambda_function_runtime": "python3.13",
+            },
+        })
+        
+        terraform.Init(t, terraformOptions)
+        terraform.Plan(t, terraformOptions)
+    })
+
+    t.Run("invalid_lambda_runtime", func(t *testing.T) {
+        terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+            TerraformDir: "../",
+            Vars: map[string]interface{}{
+                "lambda_function_runtime": "python2.7",
+            },
+        })
+        
+        _, err := terraform.InitAndPlanE(t, terraformOptions)
+        assert.Error(t, err)
+    })
+
+    t.Run("valid_cloudwatch_retention", func(t *testing.T) {
+        terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+            TerraformDir: "../",
+            Vars: map[string]interface{}{
+                "cloudwatch_log_group_retention": "30",
+            },
+        })
+        
+        terraform.Init(t, terraformOptions)
+        terraform.Plan(t, terraformOptions)
+    })
+
+    t.Run("s3_bucket_name_validation", func(t *testing.T) {
+        terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+            TerraformDir: "../",
+            Vars: map[string]interface{}{
+                "s3_bucket_name": "",
+            },
+        })
+        
+        _, err := terraform.InitAndPlanE(t, terraformOptions)
+        assert.Error(t, err)
+    })
+}
+
+func TestRequiredVariables(t *testing.T) {
+    t.Run("required_label_variables", func(t *testing.T) {
+        terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+            TerraformDir: "../",
+            Vars: map[string]interface{}{
+                "label_env":       "prod",
+                "label_namespace": "myapp",
+            },
+        })
+        
+        terraform.Init(t, terraformOptions)
+        terraform.Plan(t, terraformOptions)
+    })
 }
