4dffc434599ba4b57884e075d63fd5b3793c257d
Troy Ameigh <55170274+troy-ameigh@users.noreply.github.com>
2025-04-07 12:05:22 -0500

    made adjustments to names and typos

--- project/archives.tf
+++ project/archives.tf
@@ -1,5 +1,5 @@
 data "archive_file" "terraform-aws-lambda-layer-builder" {
   type        = "zip"
   source_dir  = "${path.module}/lambda/src/"
-  output_path = "${path.module}/.build/python-lambda-layer-builder.zip"
+  output_path = "${path.module}/.build/terraform-aws-lambda-layer-builder.zip"
 }
