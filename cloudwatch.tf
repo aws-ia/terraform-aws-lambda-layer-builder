4264a10f2680d48d1e131194ad5537543debd170
Troy Ameigh <55170274+troy-ameigh@users.noreply.github.com>
2025-04-14 15:23:28 -0500

    refactored main.tf

--- cloudwatch.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_cloudwatch_log_group" "terraform_aws_lambda_layer_builder" {
-  name              = "${local.cloudwatch.lambda_log_group_prefix}/${local.lambda.function_name}"
-  kms_key_id        = aws_kms_alias.terraform_aws_lambda_layer_builder.target_key_arn
-  retention_in_days = var.cloudwatch_log_group_retention
-
-  tags = var.tags
-}
