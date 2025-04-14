961f0af5fd48093827591f29cd3cd8f78610a35f
Troy Ameigh <55170274+troy-ameigh@users.noreply.github.com>
2025-04-14 12:48:48 -0500

    Adjusted main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "terraform_aws_lambda_layer_builder" {
-  source = "git://github.com/aws-ia/terraform-aws-lambda-layer-builder.git?ref=main"
+  # source = "git://github.com/aws-ia/terraform-aws-lambda-layer-builder.git?ref=main"
   label_env        = "dev"
   label_namespace  = "aws"
   s3_force_destroy = true
