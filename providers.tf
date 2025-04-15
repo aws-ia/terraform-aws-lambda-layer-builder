1d0ebd4356f10edd95d8fb1954479fa38a8041e8
Troy Ameigh <55170274+troy-ameigh@users.noreply.github.com>
2025-04-15 09:22:44 -0500

    updated main provider

--- providers.tf
+++ providers.tf
@@ -19,3 +19,17 @@ terraform {
     }
   }
 }
+
+variable "region" {
+  type        = string
+  description = "AWS region to deploy the resources"
+  default     = "us-east-1"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+provider "awscc" {
+  region = var.region
+}
